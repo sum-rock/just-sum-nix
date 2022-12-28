cf04a5518c55e1ea2484b0dbcedc4c743d475f4b
August Raack <sum-rock@rockyhillhouse.org>
2022-12-28 16:11:09 -0600

    Add ssh keys to nixos workstation using sops

--- secrets/workstation.nix
+++ secrets/workstation.nix
@@ -3,9 +3,21 @@
   config = {
     sops.age.keyFile = "${config.home-dir-path}/.config/sops/age/keys.txt";
     sops.defaultSopsFile = ./secrets.yaml;
-    sops.secrets.github_creds = { 
-      owner = "${config.primary-user}";
-      path = "${config.home-dir-path}/.git-credentials";
+    sops.secrets = {
+      "workstation/github_creds" = { 
+        owner = "${config.primary-user}";
+        path = "${config.home-dir-path}/.git-credentials";
+      };
+      "workstation/ssh_pvt" = {
+        owner = "${config.primary-user}";
+        mode = "0600";
+        path = "${config.home-dir-path}/.ssh/id_rsa";
+      };
+      "workstation/ssh_pub" = {
+        owner = "${config.primary-user}";
+        mode = "0644";
+        path = "${config.home-dir-path}/.ssh/id_rsa.pub";
+      };
     };
   };
 }
