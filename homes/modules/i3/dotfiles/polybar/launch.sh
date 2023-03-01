50f20df08cba228a360214c4eddd80653aac478c
august <sum-rock@rockyhillhouse.org>
2023-02-28 20:15:41 -0600

    Removes i3 because it has too many issues

--- homes/modules/i3/dotfiles/polybar/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-DIR="$HOME/.config/polybar"
-killall -q polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar -q main -c "$DIR"/config.ini &
-# for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-#   MONITOR=$m polybar --reload main &
-# done
