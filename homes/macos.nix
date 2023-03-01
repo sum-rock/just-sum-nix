f118d2ebd24f4859f1cc5acc636eeef52d25d8ef
august <sum-rock@rockyhillhouse.org>
2023-02-28 20:22:18 -0600

    Adds the editor to zshrc and cleans up expression

--- homes/macos.nix
+++ homes/macos.nix
@@ -26,37 +26,43 @@ in
   home-manager.useUserPackages = true;
 
   home-manager.users.${config.primary-user} = {
-    programs.home-manager.enable = true;
-    home.username = "${config.primary-user}";
-    home.homeDirectory = "${config.home-dir-path}";
-    home.stateVersion = "${config.nixos-version}";
-    home.file = {
-      ".wallpapers" = {
-        source = "${wallpapers.${config.theme}}";
-        recursive = true;
+    home = {
+      username = "${config.primary-user}";
+      homeDirectory = "${config.home-dir-path}";
+      stateVersion = "${config.nixos-version}";
+      file = {
+        ".wallpapers" = {
+          source = "${wallpapers.${config.theme}}";
+          recursive = true;
+        };
       };
     };
-    programs.zsh = {
-      shellAliases = {
-        yabai-rf = "launchctl kickstart -k \"gui/$UID/org.nixos.yabai\"; skhd --reload";
-        nix-edit = "cd ${config.home-dir-path}/.nixpkgs; nvim .";
-        nixos-rebuild-flake = "darwin-rebuild switch --flake ${config.home-dir-path}/.nixpkgs";
-        go-homebase = "cd ~/repositories/pyhomebase";
-        go-notes = "cd ~/Documents/notes";
-        notes = "cd ~/Documents/notes; nvim .";
-        staging-db = "aptible db:tunnel homebase-two-staging --port 52210";
-        production-db = "aptible db:tunnel homebase-two-prod --port 54171";
-        git-wipit = "git add .; git commit -m wip --no-verify";
-        git-pull-develop = "git checkout develop; git pull; git checkout -";
-        postgres-docker-up = "docker run -d -e POSTGRES_HOST_AUTH_METHOD=trust -p 5432:5432 --name test-postgres postgres";
-      };
-      initExtra = ''
-        VSCODE="/Applications/Visual Studio Code.app/Contents/Resources/app/bin" 
-        if [ -d $VSCODE ] ; then export PATH="$VSCODE:$PATH" ; fi
+    programs = {
+      home-manager.enable = true;
+      zsh = {
+        shellAliases = {
+          yabai-rf = "launchctl kickstart -k \"gui/$UID/org.nixos.yabai\"; skhd --reload";
+          nix-edit = "cd ${config.home-dir-path}/.nixpkgs; nvim .";
+          nixos-rebuild-flake = "darwin-rebuild switch --flake ${config.home-dir-path}/.nixpkgs";
+          go-homebase = "cd ~/repositories/pyhomebase";
+          go-notes = "cd ~/Documents/notes";
+          notes = "cd ~/Documents/notes; nvim .";
+          staging-db = "aptible db:tunnel homebase-two-staging --port 52210";
+          production-db = "aptible db:tunnel homebase-two-prod --port 54171";
+          git-wipit = "git add .; git commit -m wip --no-verify";
+          git-pull-develop = "git checkout develop; git pull; git checkout -";
+          postgres-docker-up = "docker run -d -e POSTGRES_HOST_AUTH_METHOD=trust -p 5432:5432 --name test-postgres postgres";
+        };
+        initExtra = ''
+          VSCODE="/Applications/Visual Studio Code.app/Contents/Resources/app/bin" 
+          if [ -d $VSCODE ] ; then export PATH="$VSCODE:$PATH" ; fi
 
-        export PATH="/opt/homebrew/opt/grep/libexec/gnubin:$PATH"
-        export DIRENV_LOG_FORMAT=""
-      '';
+          export EDITOR="nvim"
+
+          export PATH="/opt/homebrew/opt/grep/libexec/gnubin:$PATH"
+          export DIRENV_LOG_FORMAT=""
+        '';
+      };
     };
   };
 }
