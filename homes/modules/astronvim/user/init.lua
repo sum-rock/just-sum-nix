bb152649c492e43fdb1190b874580dd535fd22a1
August Raack <sum-rock@rockyhillhouse.org>
2024-03-19 20:26:29 -0500

    Add copilot

--- homes/modules/astronvim/user/init.lua
+++ homes/modules/astronvim/user/init.lua
@@ -1,17 +1,29 @@
 return {
+  colorscheme = "catppuccin",
   plugins = {
     {
       "catppuccin/nvim",
       name = "catppuccin",
       config = function()
-        require("catppuccin").setup {}
+        require("catppuccin").setup { flavour = "mocha", }
+      end,
+    },
+    {
+      "github/copilot.vim",
+      name = "copilot",
+      lazy = false,
+      config = function()
+        vim.keymap.set('i', '<C-a>', 'copilot#Accept("")', {
+          expr = true,
+          replace_keycodes = false
+        })
+        vim.g.copilot_no_tab_map = true
       end,
     },
   },
   lsp = {
     servers = {
       "pyright",
-      "lua_ls",
     },
     config = {
       pyright = {
