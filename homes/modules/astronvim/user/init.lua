bb8fe001e2ebb1f71c29bf5e7320243b121cd33c
August Raack <sum-rock@rockyhillhouse.org>
2024-03-19 20:15:05 -0500

    Add configs

--- homes/modules/astronvim/user/init.lua
+++ homes/modules/astronvim/user/init.lua
@@ -1,9 +1,22 @@
 return {
+  plugins = {
+    {
+      "catppuccin/nvim",
+      name = "catppuccin",
+      config = function()
+        require("catppuccin").setup {}
+      end,
+    },
+  },
   lsp = {
+    servers = {
+      "pyright",
+      "lua_ls",
+    },
     config = {
       pyright = {
-        reportMissingImports = false;
-      };
-    };
-  };
+        typeCheckingMode = "basic"
+      },
+    },
+  },
 }
