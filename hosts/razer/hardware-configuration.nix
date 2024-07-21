346ec0830ed0edcddf6c6fa5e00d5c4d19d65c37
August Raack <sum-rock@rockyhillhouse.org>
2024-07-20 22:27:52 -0500

    Attempting more things to fix freezing

--- hosts/razer/hardware-configuration.nix
+++ hosts/razer/hardware-configuration.nix
@@ -5,36 +5,30 @@
 
 {
   imports =
-    [
-      (modulesPath + "/installer/scan/not-detected.nix")
+    [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" ];
   boot.initrd.kernelModules = [ ];
-  # v4l2loopback module is necessary for the virtual camera in immersed
-  boot.kernelModules = [ "kvm-amd" "v4l2loopback" ];
-  boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback.out ];
-  boot.extraModprobeConfig = ''
-    options v4l2loopback card_label="Virtual Camera" exclusive_caps=1
-  '';
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    {
-      device = "/dev/disk/by-uuid/ee2498db-e023-4ce3-bfab-915fff49fb0d";
+    { device = "/dev/disk/by-uuid/ee2498db-e023-4ce3-bfab-915fff49fb0d";
       fsType = "ext4";
     };
 
   boot.initrd.luks.devices."luks-b2f449c3-abf7-4163-8e30-03a3c05c5fba".device = "/dev/disk/by-uuid/b2f449c3-abf7-4163-8e30-03a3c05c5fba";
 
   fileSystems."/boot/efi" =
-    {
-      device = "/dev/disk/by-uuid/20BF-2B1C";
+    { device = "/dev/disk/by-uuid/20BF-2B1C";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
   swapDevices =
-    [{ device = "/dev/disk/by-uuid/34f9ae78-9c5e-4a28-af57-8c8cc13624d8"; }];
+    [ { device = "/dev/disk/by-uuid/34f9ae78-9c5e-4a28-af57-8c8cc13624d8"; }
+    ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
