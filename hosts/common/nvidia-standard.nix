b9e0c90198a1990d8c0b8310fa06d42fdaaa6fe6
August Raack <sum-rock@rockyhillhouse.org>
2024-07-24 12:07:35 -0500

    Turns off power management

--- hosts/common/nvidia-standard.nix
+++ hosts/common/nvidia-standard.nix
@@ -15,7 +15,7 @@
     };
     nvidia = {
       modesetting.enable = true;
-      powerManagement.enable = true;
+      powerManagement.enable = false;
       powerManagement.finegrained = false;
       open = false;
       nvidiaSettings = true;
