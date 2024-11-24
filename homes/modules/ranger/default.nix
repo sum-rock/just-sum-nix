3c1423b72cd8e5b8e1c04af72039025b3a4e88a9
August Raack <sum-rock@rockyhillhouse.org>
2024-11-24 13:55:09 -0600

    Allow ranger to be beautiful with sudo

--- homes/modules/ranger/default.nix
+++ homes/modules/ranger/default.nix
@@ -1,13 +1,22 @@
-{ config, pkgs, home-manager, ranger-devicons, ... }: 
+{ config, pkgs, home-manager, ranger-devicons, ... }:
 {
   environment.systemPackages = with pkgs; [
     ranger
   ];
 
-  home-manager.users.${config.primary-user} = {
-    xdg.configFile = {
-      "ranger/rc.conf".source = ./rc.conf;
-      "ranger/plugins/devicons2".source = "${ranger-devicons}";
+  home-manager.users = {
+    "${config.primary-user}" = {
+      xdg.configFile = {
+        "ranger/rc.conf".source = ./rc.conf;
+        "ranger/plugins/devicons2".source = "${ranger-devicons}";
+      };
     };
   };
+  # allows sudo ranger to share the same configuration as primary user
+  system.activationScripts.ranger = ''
+    if [[ ! -d /root/.config/ranger ]]; then
+      mkdir -p /root/.config
+      ln -s /home/${config.primary-user}/.config/ranger /root/.config/ranger 
+    fi
+  '';
 }
