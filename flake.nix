8415a4502aa8bac436fa46c79c49ea91e70cf3e0
August Raack <sum-rock@rockyhillhouse.org>
2024-12-23 10:42:30 -0600

    Update to 24.11

--- flake.nix
+++ flake.nix
@@ -6,13 +6,13 @@
     # Core inputs 
     # =====================================================
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-24.05";
+      url = "github:nixos/nixpkgs/nixos-24.11";
     };
     nixpkgs-unstable = {
       url = "github:nixos/nixpkgs/nixos-unstable";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     darwin = {
