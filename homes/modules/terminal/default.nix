ad70359e0ac82aa2e1f14e6d08ba0cbf55c88a3e
August Raack <sum-rock@rockyhillhouse.org>
2025-01-26 17:09:08 -0600

    Alacritty configs
    Stop scrolling wierd

--- homes/modules/terminal/default.nix
+++ homes/modules/terminal/default.nix
@@ -6,6 +6,7 @@
 , tmux-sensible
 , tmux-open
 , tmux-catppuccin
+, alacritty-tic
 , ...
 }:
 let
@@ -14,35 +15,15 @@ let
     ${builtins.readFile "${tmux-catppuccin}/themes/catppuccin_mocha_tmux.conf"}
     run-shell $HOME/.tmux/plugins/tmux-catppuccin/catppuccin.tmux
   '';
-
-  # Tmux Color Correction
-  # =======================================================
-  # https://medium.com/@dubistkomisch/how-to-actually-get-italics-and-true-colour-to-work-in-iterm-tmux-vim-9ebe55ebc2be
-  tmux-init = pkgs.writeShellScriptBin "tmux-init" ''
-    CONF="$HOME/.tmux/setup"
-    tic -x $CONF/xterm-256color-italic.terminfo
-    tic -x $CONF/tmux-256color.terminfo
-  '';
-  xterm-italic = builtins.toFile "xterm-256color-italic.terminfo" ''
-    xterm-256color-italic|xterm with 256 colors and italic,
-      sitm=\E[3m, ritm=\E[23m,
-      use=xterm-256color,
-  '';
-  tmux-color = builtins.toFile "tmux-256color.terminfo" ''
-    tmux-256color|tmux with 256 colors,
-      ritm=\E[23m, rmso=\E[27m, sitm=\E[3m, smso=\E[7m, Ms@,
-      khome=\E[1~, kend=\E[4~,
-      use=xterm-256color, use=screen-256color,
-  '';
-
 in
 {
+  system.activationScripts.alacritty-tic = {
+    text = "${pkgs.ncurses5}/bin/tic -o${config.home-dir-path}/.terminfo -x ${alacritty-tic}/extra/alacritty.info";
+  };
   environment.systemPackages = [
     pkgs.tmux
     pkgs.eza
-    pkgs.alacritty
     pkgs.starship
-    tmux-init
   ];
 
   home-manager.users.${config.primary-user} = {
@@ -64,8 +45,6 @@ in
     };
     home.file = {
       ".tmux.conf".source = "${tmux-conf}";
-      ".tmux/setup/xterm-256color-italic.terminfo".source = "${xterm-italic}";
-      ".tmux/setup/tmux-256color.terminfo".source = "${tmux-color}";
       ".tmux/plugins/tmux-copycat" = {
         source = "${tmux-copycat}";
         recursive = true;
