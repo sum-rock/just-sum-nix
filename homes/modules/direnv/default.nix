b826883ad17b9dffa49e06ab7f68739c6151e656
August Raack <sum-rock@rockyhillhouse.org>
2026-04-24 11:16:27 -0500

    Real Fix?
    squash

--- homes/modules/direnv/default.nix
+++ homes/modules/direnv/default.nix
@@ -1,13 +1,5 @@
 { config, pkgs, home-manager, ... }:
 {
-  nixpkgs.overlays = [
-    (final: prev: {
-      direnv = prev.direnv.overrideAttrs (_: {
-        # this is here to address build errors on macos. TODO: remove when resolved 
-        doCheck = false;
-      });
-    })
-  ];
   home-manager.users.${config.primaryUser}.programs = {
     direnv = {
       enable = true;
