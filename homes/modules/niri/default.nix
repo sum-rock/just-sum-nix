6d0d26b4055b10a6f80f88e05d7432e4d9bdd724
August Raack <sum-rock@rockyhillhouse.org>
2026-06-25 19:14:16 -0500

    Adds Niri WM

---
+++ homes/modules/niri/default.nix
@@ -0,0 +1,99 @@
+{ pkgs, config, ... }:
+{
+  programs.niri.enable = true;
+
+  security.polkit.enable = true;
+  security.pam.services.swaylock = { };
+
+  services = {
+    libinput.enable = true;
+    gnome.gnome-keyring.enable = true;
+    displayManager.gdm.enable = false;
+    greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command = "${pkgs.niri}/bin/niri-session";
+          user = config.primaryUser;
+        };
+      };
+    };
+    xserver = {
+      enable = true;
+      xkb.layout = "us";
+    };
+  };
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-gnome
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    niri
+
+    walker
+    swaylock
+    swayidle
+    awww
+
+    wl-clipboard
+    cliphist
+
+    grim
+    slurp
+
+    nwg-look
+    nwg-displays
+
+    polkit_gnome
+
+    adwaita-icon-theme
+    catppuccin-gtk
+    catppuccin-cursors.mochaDark
+  ];
+
+  home-manager.users.${config.primaryUser} = {
+    home.packages = with pkgs; [
+      foot
+      nautilus
+      pavucontrol
+      networkmanagerapplet
+      blueman
+    ];
+
+    gtk = {
+      enable = true;
+      theme = {
+        name = "catppuccin-mocha-mauve-standard";
+        package = pkgs.catppuccin-gtk.override {
+          accents = [ "mauve" ];
+          size = "standard";
+          tweaks = [ "normal" ];
+          variant = "mocha";
+        };
+      };
+      cursorTheme = {
+        name = "catppuccin-mocha-dark-cursors";
+        package = pkgs.catppuccin-cursors.mochaDark;
+      };
+      iconTheme.name = "Adwaita";
+    };
+
+    home.pointerCursor = {
+      gtk.enable = true;
+      name = "catppuccin-mocha-dark-cursors";
+      package = pkgs.catppuccin-cursors.mochaDark;
+      size = 24;
+    };
+
+    xdg.configFile."niri/config.kdl".text = builtins.readFile ./niri.kdl;
+    xdg.configFile."foot/foot.ini".text = builtins.readFile ./foot.ini;
+    xdg.configFile."swaylock/config".text = builtins.readFile ./swaylock.ini;
+    xdg.configFile."walker/config.toml".text = builtins.readFile ./walker.toml;
+    # xdg.configFile."walker/themes/catppuccin-mocha.css".text = builtins.readFile ./walker.css;
+  };
+}
